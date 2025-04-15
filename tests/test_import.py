329cfa0cd988eacde4853a618cd4bafc64c663f7
Matthew Vine <32849887+MattTheCuber@users.noreply.github.com>
2025-04-15 00:22:22 +0000

    test: fix import tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,7 +1,7 @@
 def test_import():
-    from trame_golden_layout.widgets.trame_golden_layout import (
-        CustomWidget,
+    from trame_golden_layout.widgets.glayout import (
+        GoldenLayout,
     )  # noqa: F401
 
-    # For components only, the CustomWidget is also importable via trame
-    from trame.widgets.trame_golden_layout import CustomWidget  # noqa: F401,F811
+    # For components only, the GoldenLayout is also importable via trame
+    from trame.widgets.glayout import GoldenLayout  # noqa: F401,F811
